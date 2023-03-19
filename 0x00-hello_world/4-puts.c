1c2b11c8193ddecc82943eb224072fb7240c7ba7
stef nakaqo <hnarruto@gmail.com>
2023-03-19 12:46:51 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
-int main(void) { puts("Programming is like building a multilingual puzzle"); return 0; }
+
+/*
+
+Print String using "puts"
+*/
+int main(void) {
+  puts("Programming is like building a multilingual puzzle");
+  return 0;
+}
