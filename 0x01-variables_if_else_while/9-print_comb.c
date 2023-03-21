23c012dd788608bad5cce8c2bbc013fe84511826
stef nakaqo <hnarruto@gmail.com>
2023-03-20 18:47:55 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,18 @@
 
 int main(void)
 {
-        int i = 0;
+	int i = 0;
 
-        while (i < 10)
-        {
-                putchar(i + '0');
-				putchar(', ');
-                i++;
-        }
-        putchar('\n');
-        return (0);
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i != 9)
+				{
+		putchar(',');
+		putchar(' ');
+				}
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
