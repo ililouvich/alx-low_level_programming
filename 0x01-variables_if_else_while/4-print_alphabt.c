478e74b5b2e1a08508685f7d38370e167c8da70d
stef nakaqo <hnarruto@gmail.com>
2023-03-20 18:07:03 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints the alphabet in lowercase, and then in uppercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if(i != 'e' && c!= 'q')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
