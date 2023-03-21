ed21f24b8854e1033a299b29ed9cd4b1842988c7
stef nakaqo <hnarruto@gmail.com>
2023-03-20 18:17:47 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,19 +2,19 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 
 
 
-int main(void) {
-    int i;
+int main(void)
+{
+	int i;
 
-    for (i = 0; i < 10; i++) {
-        putchar(i + '0');
-    }
-    putchar('\n');
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	putchar('\n');
 
-    return 0;
+	return (0);
 }
