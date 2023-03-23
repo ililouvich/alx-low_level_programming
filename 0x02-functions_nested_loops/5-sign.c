940087ab81c1be123434df744297edb5170fa24a
stef nakaqo <hnarruto@gmail.com>
2023-03-22 18:09:54 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+/**
+ * print_sign - Prints the sign of a number
+ * @n: The number to check
+ *
+ * Return: 1 if n > zero, 0 if n = zero, -1 if n < zero
+ */
+int print_sign(int n)
+
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
