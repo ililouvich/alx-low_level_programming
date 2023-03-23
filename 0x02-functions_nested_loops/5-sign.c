5078e398ba30f111a03709e1f7520d24c5210ff9
stef nakaqo <hnarruto@gmail.com>
2023-03-22 18:17:27 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,21 +5,20 @@
  * Return: 1 if n > zero, 0 if n = zero, -1 if n < zero
  */
 int print_sign(int n)
-
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
 }
